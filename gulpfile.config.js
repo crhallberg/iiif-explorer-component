e0bb76b490c215da2d2d09e7588de7ef4c6cea2b
edsilv <e.silverton@gmail.com>
2016-09-04 16:33:19 +0100

    improved config structure

--- gulpfile.config.js
+++ gulpfile.config.js
@@ -3,49 +3,66 @@ var metadata = require('./package');
 var GulpConfig = (function () {
     function GulpConfig() {
         this.name = metadata.name;
-        // libs that MUST be included in a consuming app for this component to work
-        this.deps = [
-            'node_modules/base-component/dist/base-component.bundle.js'
-        ];
-        // libs that MAY be included in a consuming app but are used here for examples purposes
-        this.examplesDeps = [
-            'node_modules/manifold/dist/manifold.bundle.js'
-        ];
-        this.examplesDir = './examples';
-        this.examplesDepsDir = './examples/js';
-        // ts definitions to copy to the typings dir
-        // ts definitions to copy to the typings dir
-        this.typings = [
-            'node_modules/base-component/dist/base-component.d.ts',
-            'node_modules/manifold/dist/manifold.bundle.d.ts',
-            'node_modules/utils/dist/utils.d.ts'
-        ];
-        this.typingsDir = './typings';
-        this.dist = './dist';
         this.header = '// ' + this.name + ' v' + metadata.version + ' ' + metadata.homepage + '\n';
-        this.jsOut = this.name + '.js';
-        this.jsMinOut = this.name + '.min.js';
-        this.jsBundleOut = this.name + '.bundle.js';
-        this.dtsOut = this.name + '.d.ts';
-        this.dtsBundleOut = this.name + '.bundle.d.ts';
-        this.tsSrc = [
-            'src/_references.ts',
-            'src/*.ts',
-            'typings/*.ts',
-            'typings/**/*.ts'];
-        this.tsConfig = {
-            declarationFiles: true,
-            noExternalResolve: true,
-            noLib: false,
-            module: 'commonjs',
-            sortOutput: true
+        this.dependencies = {
+            // libs that MUST be included in a consuming app for this component to work
+            libs: [
+                'node_modules/base-component/dist/base-component.bundle.js',
+                'node_modules/jquery-plugins/dist/jquery-plugins.js',
+                'node_modules/manifold/dist/manifold.bundle.js'
+            ],
+            // libs that MAY be included in a consuming app but are used here for example purposes
+            examples: [],
+            // ts definitions to copy to the typings dir
+            typings: [
+                'node_modules/base-component/dist/base-component.d.ts',
+                'node_modules/manifold/dist/manifold.bundle.d.ts',
+                'node_modules/utils/dist/utils.d.ts'
+            ]
         };
-        this.browserifyConfig = {
-            standalone: this.name,
-            debug: false
+        this.fileNames = {
+            cssOut: this.name + '.css',
+            jsOut: this.name + '.js',
+            jsMinOut: this.name + '.min.js',
+            jsBundleOut: this.name + '.bundle.js',
+            dtsOut: this.name + '.d.ts',
+            dtsBundleOut: this.name + '.bundle.d.ts'
         };
-        this.browserifySrc = this.dist;
-        this.browserifyTarget = this.dist;
+        this.directories = {
+            dist: './dist',
+            examples: './examples',
+            examplesCss: './examples/css',
+            examplesImg: './examples/img',
+            examplesJs: './examples/js',
+            typings: './typings'
+        };
+        this.typescript = {
+            src: [
+                'src/_references.ts',
+                'src/*.ts',
+                'typings/*.ts',
+                'typings/**/*.ts'
+            ],
+            config: {
+                declarationFiles: true,
+                noExternalResolve: true,
+                noLib: false,
+                module: 'commonjs',
+                sortOutput: true
+            }
+        };
+        this.browserify = {
+            src: this.directories.dist,
+            target: this.directories.dist,
+            config: {
+                standalone: this.name,
+                debug: false
+            }
+        };
+        this.sources = {
+            css: 'src/css/**/*.less',
+            img: './src/img/**'
+        };
     }
     return GulpConfig;
 })();
